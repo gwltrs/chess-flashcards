05b08d2278524de5a0afdc7d1efd144734146e9a
Gregory Walters <waltersge1@gmail.com>
2020-09-24 12:33:52 -0400

    added chess types

--- src/Main.purs
+++ src/Main.purs
@@ -15,33 +15,55 @@ main = HA.runHalogenAff do
   body <- HA.awaitBody
   runUI component unit body
 
-type State = Int
+type State = { puzzles :: Array Puzzle, reviewStack :: Array PuzzleName, view :: View }
+type Puzzle = 
+  { 
+    name :: PuzzleName,
+    fen :: String,
+    move :: Move,
+    box :: Int,
+    lastDrilledAt :: Int
+  }
+type PuzzleName = String
+type Move = { from :: Space, to :: Space, underpromotion :: Maybe Underpromotion }
+type Space = String -- In the format of 'a8', 'h4', 'f3', etc.
+data Underpromotion = Bishop | Knight | Rook
+data View = LoadingFile | MainMenu
 
-data Action = Increment | Decrement
+data Action = NewFile | LoadFile
 
 component :: forall q i o m. H.Component HH.HTML q i o m
 component =
   H.mkComponent
-    { initialState
-    , render
-    , eval: H.mkEval H.defaultEval { handleAction = handleAction }
+    { 
+      initialState, 
+      render, 
+      eval: H.mkEval H.defaultEval { handleAction = handleAction }
     }
 
 initialState :: forall i. i -> State
-initialState _ = 0
+initialState _ = { puzzles: [], reviewStack: [], view: LoadingFile }
 
 render :: forall m. State -> H.ComponentHTML Action () m
 render state =
-  HH.div_
-    [ HH.button [ HE.onClick \_ -> Just Decrement ] [ HH.text "-" ]
-    , HH.text (show state)
-    , HH.button [ HE.onClick \_ -> Just Increment ] [ HH.text "+" ]
-    ]
+  case state.view of
+    LoadingFile -> 
+      HH.div_
+        [ 
+          HH.button [ HE.onClick \_ -> Just NewFile ] [ HH.text "New" ], 
+          HH.button [ HE.onClick \_ -> Just LoadFile ] [ HH.text "Load" ]
+        ]
+    MainMenu -> 
+      HH.div_
+        [ 
+          HH.button [ HE.onClick \_ -> Just NewFile ] [ HH.text "Save" ],
+          HH.button [ HE.onClick \_ -> Just LoadFile ] [ HH.text "Review" ],
+          HH.button [ HE.onClick \_ -> Just LoadFile ] [ HH.text "Create" ]
+        ]
 
 handleAction :: forall o m. Action -> H.HalogenM State Action () o m Unit
 handleAction = case _ of
-  Decrement ->
-    H.modify_ \state -> state - 1
-
-  Increment ->
-    H.modify_ \state -> state + 1
+  NewFile ->
+    H.modify_ \state -> state { view = MainMenu }
+  LoadFile ->
+    H.modify_ \state -> state { view = MainMenu }
