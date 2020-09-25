ded460bdf427001485de9f9937f5e5276bfb4b5e
Gregory Walters <waltersge1@gmail.com>
2020-09-25 16:54:14 -0400

    installed purescript-test-unit; tests incoming

--- test/Main.purs
+++ test/Main.purs
@@ -5,7 +5,15 @@ import Prelude
 import Effect (Effect)
 import Effect.Class.Console (log)
 
+import Test.Unit (suite, test)
+import Test.Unit.Main (runTest)
+import Test.Unit.Assert as Assert
+
 main :: Effect Unit
-main = do
-  log "🍝"
-  log "You should add some tests."
+main = runTest do
+  suite "sync code" do
+    test "arithmetic" do
+      Assert.assert "2 + 2 should be 4" $ (2 + 2) == 4
+      Assert.assertFalse "2 + 2 shouldn't be 5" $ (2 + 2) == 5
+      Assert.equal 4 (2 + 2)
+      Assert.expectFailure "2 + 2 shouldn't be 5" $ Assert.equal 5 (2 + 2)
