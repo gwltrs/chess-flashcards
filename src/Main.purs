47c0386f4ffec7c96c649950623763bb3f198d93
Gregory Walters <waltersge1@gmail.com>
2020-09-26 14:27:21 -0400

    moved render into a separate file

--- src/Main.purs
+++ src/Main.purs
@@ -12,6 +12,7 @@ import Halogen.VDom.Driver (runUI)
 
 import Types
 import Reducer (reducer)
+import Render
 
 main :: Effect Unit
 main = HA.runHalogenAff do
@@ -30,36 +31,6 @@ component =
 initialState :: forall i. i -> State
 initialState _ = { puzzles: [], reviewStack: [], view: LoadingFile }
 
-render :: forall m. State -> H.ComponentHTML Action () m
-render state =
-  case state.view of
-    LoadingFile -> 
-      HH.div_
-        [ 
-          HH.button [ HE.onClick \_ -> Just NewFile ] [ HH.text "New" ],
-          HH.button [ HE.onClick \_ -> Just LoadFile ] [ HH.text "Load" ]
-        ]
-    MainMenu _ _ -> 
-      HH.div_
-        [ 
-          HH.button [ HE.onClick \_ -> Just SaveFile ] [ HH.text "Save" ],
-          HH.br_,
-          HH.button [ HE.onClick \_ -> Just Review ] [ HH.text "Review" ],
-          HH.br_,
-          HH.input [ HE.onValueChange \val -> Just (UpdatePuzzleName val) ],
-          HH.input [ HE.onValueChange \val -> Just (UpdateFEN val) ],
-          HH.button [ HE.onClick \_ -> Just CreatePuzzle ] [ HH.text "Create" ]
-        ]
-    CreatingPuzzle -> 
-      HH.div_
-        [ 
-          HH.button [ HE.onClick \_ -> Just BackToMain ] [ HH.text "Back" ],
-          HH.button [ HE.onClick \_ -> Just SavePuzzle ] [ HH.text "Save" ]
-        ]
-
 handleAction :: forall o m. Action -> H.HalogenM State Action () o m Unit
 handleAction action =
   H.modify_ \state -> reducer state action
-
-asdf :: Int -> Int
-asdf x = x + 1
---
+++ src/Render.purs
@@ -0,0 +1,35 @@
+module Render where
+
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Data.Maybe (Maybe(..))
+
+import Types
+  
+render :: forall m. State -> H.ComponentHTML Action () m
+render state =
+  case state.view of
+    LoadingFile -> 
+      HH.div_
+        [ 
+          HH.button [ HE.onClick \_ -> Just NewFile ] [ HH.text "New" ],
+          HH.button [ HE.onClick \_ -> Just LoadFile ] [ HH.text "Load" ]
+        ]
+    MainMenu _ _ -> 
+      HH.div_
+        [ 
+          HH.button [ HE.onClick \_ -> Just SaveFile ] [ HH.text "Save" ],
+          HH.br_,
+          HH.button [ HE.onClick \_ -> Just Review ] [ HH.text "Review" ],
+          HH.br_,
+          HH.input [ HE.onValueChange \val -> Just (UpdatePuzzleName val) ],
+          HH.input [ HE.onValueChange \val -> Just (UpdateFEN val) ],
+          HH.button [ HE.onClick \_ -> Just CreatePuzzle ] [ HH.text "Create" ]
+        ]
+    CreatingPuzzle -> 
+      HH.div_
+        [ 
+          HH.button [ HE.onClick \_ -> Just BackToMain ] [ HH.text "Back" ],
+          HH.button [ HE.onClick \_ -> Just SavePuzzle ] [ HH.text "Save" ]
+        ]
