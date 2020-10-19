8a8644945e3db1658b7e1727f87b715a337e2127
Gregory Walters <waltersge1@gmail.com>
2020-10-19 17:38:04 -0400

    Added PuzzlesJSON module

---
+++ src/PuzzlesJSON.purs
@@ -0,0 +1,16 @@
+module PuzzlesJSON where
+
+import Data.Maybe (Maybe(..))
+
+import Types (Puzzle)
+
+-- Wrapper interfaces for JSON parsing/serialization.
+-- Even though tested lib(s) will likely be imported,
+-- defining these first are necessary for implementing
+-- roundtrip tests in a TDD sequence (writing tests first).
+
+makePuzzlesJSON :: Array Puzzle -> String
+makePuzzlesJSON puzzles = ""
+
+parsePuzzlesJSON :: String -> Maybe (Array Puzzle)
+parsePuzzlesJSON jsonString = Nothing
